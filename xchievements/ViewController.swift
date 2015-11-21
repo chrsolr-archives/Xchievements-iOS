3d01da6d65a5cd1068221930562e2b6cea90d28d
Christian Soler <twenty40@gmail.com>
2015-11-21 10:21:33 -0600

    Initial Commit

---
+++ xchievements/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  xchievements
+//
+//  Created by Christian Soler on 11/21/15.
+//  Copyright © 2015 Christian Soler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
